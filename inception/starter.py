ec4de003ea76d60c5ecc30ae3fad392766d7acf9
Shruti Dhoot <shruti@gab-test.novalocal>
2015-11-01 12:31:41 +0000

    Added gabriel to the project, modified the starter script to read from a folder and modified gabriel-proxy to write to a folder

--- inception/starter.py
+++ inception/starter.py
@@ -1,11 +1,7 @@
-# from IPython import display
-from matplotlib import pyplot
 import numpy as np
 import os
 import sys
-# %matplotlib inline
 import pdb
-
 
 # Make sure that you set this to the location your caffe2 library lies.
 caffe2_root = '/home/shruti/caffe2'
@@ -13,7 +9,7 @@ sys.path.insert(0, os.path.join(caffe2_root, 'gen'))
 
 # After setting the caffe2 root path, we will import all the caffe2 libraries needed.
 from caffe2.proto import caffe2_pb2
-from pycaffe2 import visualize, core, workspace, net_drawer
+from pycaffe2 import workspace #net_drawer, visualize, core
 
 # net is the network definition.
 net = caffe2_pb2.NetDef()
@@ -22,21 +18,9 @@ net.ParseFromString(open('inception_net.pb').read())
 tensors = caffe2_pb2.TensorProtos()
 tensors.ParseFromString(open('inception_tensors.pb').read())
 
-# Note that the following line hides the intermediate blobs and only shows the operators.
-# If you want to show all the blobs as well, use the commented GetPydotGraph line.
-graph = net_drawer.GetPydotGraphMinimal(net.op, name="inception", rankdir='TB')
-# graph = net_drawer.GetPydotGraph(net.op, name="inception", rankdir='TB')
-
-#display.Image(graph.create_png(), width=200)
-
 DEVICE_OPTION = caffe2_pb2.DeviceOption()
 # Let's use CPU in our example.
 DEVICE_OPTION.device_type = caffe2_pb2.CPU
-
-# If you have a GPU and want to run things there, uncomment the below two lines.
-# If you have multiple GPUs, you also might want to specify a gpu id.
-# DEVICE_OPTION.device_type = caffe2_pb2.CUDA
-# DEVICE_OPTION.cuda_gpu_id = 0
 
 # Caffe2 has a concept of "workspace", which is similar to that of Matlab. Each workspace
 # is a self-contained set of tensors and networks. In this case, we will just use the default
@@ -55,7 +39,7 @@ net.device_option.CopyFrom(DEVICE_OPTION)
 workspace.CreateNet(net)
 
 
-def ClassifyImageWithInception(image_file, show_image=False, output_name="softmax2"):
+def ClassifyImageWithInception(image_file, output_name="softmax2"):
     from skimage import io, transform
     img = io.imread(image_file)
     # Crop the center
@@ -65,8 +49,6 @@ def ClassifyImageWithInception(image_file, show_image=False, output_name="softma
     cropped_img = img[crop_height:crop_height + shorter_edge, crop_width:crop_width + shorter_edge]
     # Resize the image to 224 * 224
     resized_img = transform.resize(cropped_img, (224, 224))
-    if show_image:
-        pyplot.imshow(resized_img)
     # normalize the image and feed it into the network. The network expects
     # a four-dimensional tensor, since it can process images in batches. In our
     # case, we will basically make the image as a batch of size one.
@@ -91,20 +73,10 @@ def detectObjects(filename):
         print '%6d (prob %.4f) synset %s' % (idx, predictions[idx], synsets[idx])
 
 
-detectObjects(sys.argv[1])
-#detectObjects(sys.argv[2])
-#pyplot.plot(predictions)
-
-#filters = workspace.FetchBlob('conv2d0_w')
-# We normalize the filters for visualization.
-#filters = (filters - filters.min()) / (filters.max() - filters.min())
-#_ = visualize.PatchVisualizer(gap=2).ShowMultiple(filters, bg_func=np.max)
-
-#conv2d0 = workspace.FetchBlob('conv2d0')
-#print 'First layer output shape:', conv2d0.shape
-#_ = visualize.PatchVisualizer(gap=10).ShowChannels(conv2d0[0, :, :, :16], bg_func=np.max, cmap=pyplot.cm.hot)
-
-#mixed5b = workspace.FetchBlob('mixed5b')
-#print 'First layer output shape:', mixed5b.shape
-#visualize.ShowChannels(mixed5b[0, :, :, :144], bg_func=np.max, cmap=pyplot.cm.hot)
-
+directory='/home/shruti/15821/output'
+# Walk the tree.
+for root, directories, files in os.walk(directory):
+    for filename in files:
+    # Join the two strings in order to form the full filepath.
+        filepath = os.path.join(root, filename)
+	detectObjects(filepath)
